d460cf46d913f14257b83c069de44d438bcec0a7
yangzp <1784956734@qq.com>
2022-10-24 23:38:09 +0800

    initProject

--- src/main/java/com/yzp/entity/SeckillGoods.java
+++ src/main/java/com/yzp/entity/SeckillGoods.java
@@ -3,21 +3,22 @@ package com.yzp.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 /**
-* <p>
-* 
-* </p>
-*
-* @author yangzp
-* @since 2022-10-24
-*/
+ * <p>
+ *
+ * </p>
+ *
+ * @author yangzp
+ * @since 2022-10-24
+ */
 @Data
 @TableName("t_seckill_goods")
 @ApiModel(value = "SeckillGoods对象", description = "")
@@ -26,7 +27,7 @@ public class SeckillGoods implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("秒杀商品ID")
-           @TableId(value = "id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     @ApiModelProperty("商品ID")
